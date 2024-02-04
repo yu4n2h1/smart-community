ebae50c566765c28802f2c0a83076ec80b61cc5e
yuanzhimengbian <x19511603116@163.com>
2024-02-04 12:22:24 +0800

    添加标签导航栏

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,10 +1,19 @@
 <template>
-	<view class="uni-margin-wrap">
+	
+	<view class="sw-view">
 		<swiper class="sw-cont" indicator-dots="true" indicator-active-color="#22CDEE" circular="true" autoplay="true">
-			<swiper-item class="sw-item" v-for="(item,index) in swiperArr" :key="index">
+			<swiper-item style="text-align: center" class="sw-item" v-for="(item,index) in swiperArr" :key="index">
 				<image class="sw-img" :src="item"></image>
 			</swiper-item>
 		</swiper>
+	</view>
+	
+	<!-- 导航菜单 -->
+	<view id="navView">
+	  <view class="navItemView" v-for="(item, index) in navItems" :key="index">
+		<image :src="item.imgSrc"></image>
+		<text>{{ item.text }}</text>
+	  </view>
 	</view>
 </template>
 
@@ -16,33 +25,52 @@
 					'/static/img/swiper3.jpg',
 					'/static/img/swiper4.jpg'
 				];
+	const navItems = [
+		{"imgSrc": "/static/img/home.png" , "text": "心理测评"},
+		{"imgSrc": "/static/img/home-select.png" , "text": "咨询预约"},
+		{"imgSrc": "/static/img/me.png" , "text": "心理答疑"},
+		{"imgSrc": "/static/img/me-select.png" , "text": "心理知识"},
+		{"imgSrc": "/static/img/service.png" , "text": "FM"},
+		{"imgSrc": "/static/img/service-select.png" , "text": "公益中心"},
+	]
 </script>
 
 <style>
-	.uni-margin-wrap {
-		width: calc(351rpx * 2);
-		width: 100%;
-		border: 10rpx;
-		
+	.sw-view {
+		height: 197px;
 	}
 	
 	.sw-cont {
 		height: 197px;
-		weight: 351px;
-	}
-	
-	.sw-item {
-		display: block;
-		height: calc(197rpx * 2);
-		line-height: calc(197rpx * 2);
-		text-align: center;
+		width: 375px;
 	}
 
 	.sw-item .sw-img {
-		width: calc(351rpx * 2);
-		height: calc(197rpx * 2);
+		width: 93%;
+		height: 100%;
 		border-radius: 20rpx 20rpx 20rpx 20rpx;
 	}
 	
-
+	.dh-view {
+		height: 190px;
+		width: 100%;
+	}
+	
+	#navView{
+	  display: flex;  /* 应用flex布局 ，所有的子元素排在一行*/
+	  flex-wrap: wrap; /* 换行*/
+	  height: 380rpx;
+	  align-content: space-around; /* 多行垂直排列*/
+	  font-size: 20rpx;
+	  font-weight: 600;
+	}
+	.navItemView{
+	  width: 87rpx;
+	  text-align: center;
+	  margin: 0 50rpx;
+	}
+	.navItemView > image{
+	  width: 87rpx;
+	  height: 87rpx;
+	}
 </style>
