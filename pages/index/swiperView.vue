786cd3ba098688c57325a8747ae697b97aa3fa60
zishuQ <1574908708@qq.com>
2024-02-06 00:19:41 +0800

    修改公告栏样式，修改轮播图大小适配 (#3)
    Co-authored-by: zishuQ <zishuq77@foxmail.com>

--- pages/index/swiperView.vue
+++ pages/index/swiperView.vue
@@ -20,29 +20,25 @@
 <style>
 	.sw-view {
 		height: calc(197rpx * 2);
+		width: 100%;
 	}
-	
+
 	.sw-cont {
 		height: calc(197rpx * 2);
-		width: 100%;
-		
 	}
+
 	.sw-item {
-		display: flex;
+		display: block;
+		height: calc(197rpx * 2);
+		line-height: calc(197rpx * 2);
 		align-items: center;
 		justify-content: center;
 	}
+
 	.sw-item .sw-img {
-		width: 93%;
-		height: 94%;
-		border-radius: 20rpx 20rpx 20rpx 20rpx;
+		width: calc(351rpx * 2);
+		height: calc(197rpx * 2);
+		border-radius: 20rpx;
 		box-shadow: 0 1rpx 12rpx 3rpx #a8a8a8;
-	}
-	
-	.dh-view {
-		height: calc(190rpx * 2);
-		width: 100%;
-		
-	
 	}
 </style>
