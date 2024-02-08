9e23d82fd7c71f4dc822e45cc6530c7d112306b8
zishuQ <1574908708@qq.com>
2024-02-08 22:22:25 +0800

    微信设置按需注入，调整px为rpx (#15)
    Co-authored-by: zishuQ <zishuq77@foxmail.com>

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -1,13 +1,13 @@
 <template>
 	<view style="background-color: #e6e6e6; height: 100vh;">
-		<view style="display: flex; flex-direction: row; align-items: center; margin-bottom: 10px;" class="top-block">
+		<view style="display: flex; flex-direction: row; align-items: center; margin-bottom: calc(10rpx * 2);" class="top-block">
 			<view>
-				<up-image :show-loading="true" style="margin-left: 15px; margin-right: 15px;"
+				<up-image :show-loading="true" style="margin-left: calc(15rpx * 2); margin-right: calc(15rpx * 2);"
 					src="../../static/img/logo.png" width="130rpx" height="130rpx" @click="click"
 					shape="circle"></up-image>
 			</view>
 			<view>
-				<text style="color: white; font-size: 25px;">嘎嘎</text>
+				<text style="color: white; font-size: calc(25rpx * 2);">嘎嘎</text>
 			</view>
 			<view style="margin-left: auto;">
 				<u-icon name="arrow-right"></u-icon>
@@ -17,22 +17,22 @@
 			<view style="display: flex; justify-content: center;">
 				<view class="line-block">
 
-					<view style="border-bottom: 1px solid #ccc; padding: 20px;">
+					<view style="border-bottom: 1px solid #ccc; padding: calc(20rpx * 2);">
 						<text style="display: inline-block; float: left;">我的信息</text>
 						<u-icon name="arrow-right" style="display: inline-block; float: right;"></u-icon>
 					</view>
 
-					<view style="border-bottom: 1px solid #ccc; padding: 20px;">
+					<view style="border-bottom: 1px solid #ccc; padding: calc(20rpx * 2);">
 						<text style="display: inline-block; float: left;">维修记录</text>
 						<u-icon name="arrow-right" style="display: inline-block; float: right;"></u-icon>
 					</view>
 
-					<view style="border-bottom: 1px solid #ccc; padding: 20px;">
+					<view style="border-bottom: 1px solid #ccc; padding: calc(20rpx * 2);">
 						<text style="display: inline-block; float: left;">投诉建议</text>
 						<u-icon name="arrow-right" style="display: inline-block; float: right;"></u-icon>
 					</view>
 
-					<view style="border-bottom: 1px solid #ccc; padding: 20px;">
+					<view style="border-bottom: 1px solid #ccc; padding: calc(20rpx * 2);">
 						<text style="display: inline-block; float: left;">我的房产</text>
 						<u-icon name="arrow-right" style="display: inline-block; float: right;"></u-icon>
 					</view>
@@ -40,10 +40,9 @@
 				</view>
 			</view>
 
-			<view style="display: flex; justify-content: center; margin-top: 15px;">
+			<view style="display: flex; justify-content: center; margin-top: calc(15rpx * 2);">
 				<view class="line-block">
-
-					<view style="border-bottom: 1px solid #ccc; padding: 20px;">
+					<view style="border-bottom: 1px solid #ccc; padding: calc(20rpx * 2);">
 						<text style="display: inline-block; float: left;">设置</text>
 						<u-icon name="arrow-right" style="display: inline-block; float: right;"></u-icon>
 					</view>
@@ -69,7 +68,6 @@
 		/* 设置宽度 */
 		background-color: rgba(255, 255, 255, 0.8);
 		/* 设置透明度为0.8的白色背景 */
-
 	}
 
 	.top-block {
