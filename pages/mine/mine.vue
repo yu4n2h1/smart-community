0a0f9c912ba5de414e32bbea47fcff1a7641e832
张海龙 <772864618@qq.com>
2024-02-24 16:26:23 +0800

    新的更新

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -1,67 +1,16 @@
 <template>
 	<view style="background-color: #e6e6e6; height: 100vh;">
-
-
+		<topBlock></topBlock>	<!-- 顶部栏 -->
+		<lineBlock></lineBlock>	<!-- 信息列表 -->
 	</view>	
 
 </template>
 
 <style lang="scss">
-<<<<<<< .mine
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=======
-	.line-block {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		// align-items: center;
-		border-radius: calc(15rpx * 2);
-		/* 调整圆角的大小 */
-		overflow: hidden;
-		/* 防止子元素溢出圆角区域 */
-		// background-color: #FFFFFF; /* 设置背景色 */
-		width: 96%;
-		/* 设置宽度 */
-		background-color: rgba(255, 255, 255, 0.8);
-		/* 设置透明度为0.8的白色背景 */
-	}
->>>>>>> .theirs
-
-<<<<<<< .mine
-
-
-
-
-
-
-
-=======
-	.top-block {
-		width: 100%;
-		height: calc(205rpx * 2);
-		background: linear-gradient(to bottom, #87CEEB, #ADD8E6, #B0E0E6, #B0E0E6, #CAE1FF);
-		border-bottom-left-radius: calc(20rpx * 2);
-		border-bottom-right-radius: calc(20rpx * 2);
-	}
->>>>>>> .theirs
 </style>
 
 <script setup>
-<<<<<<< .mine
 	import topBlock from "@/pages/mine/topBlock.vue"
 	import lineBlock from "@/pages/mine/lineBlock.vue"
 	
@@ -77,20 +26,3 @@
 	// });	
 </script>
 
-=======
-</script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> .theirs
