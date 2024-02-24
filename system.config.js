06363f791a79ef0ad909ba9c0c5b7bdb371ee995
远志 <93107257+yuanzhimengbian@users.noreply.github.com>
2024-02-24 16:07:04 +0800

    冲突合并演示

--- system.config.js
+++ system.config.js
@@ -3,6 +3,6 @@ export default{
 	// 开发 测试 用
 	developUrl: 'http://[240e:345:5e40:6c00:e96f:8015:a7ef:9d19]:8081',
 	fileUrl: 'http://localhost:8899/files',
-	// 
+	// main 分支提交的测试数据: 
 	baseUrl: ''
 }
