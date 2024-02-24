0f669a0ae0dfb63a45272654b1d21ca5c814ab8f
imLightning <2744002576@qq.com>
2024-02-24 23:04:39 +0800

    更新问卷发布，优化显示

--- pages/questionnaire/questionnaireAdmin.vue
+++ pages/questionnaire/questionnaireAdmin.vue
@@ -1,21 +1,24 @@
 <!-- 
 	问卷内容编辑页
-	
  -->
-
 <template>
 	<view class="W">
 		<view class="header">
-			<input class="header-tit" type="text" placeholder="在此输入题目" v-model="data.name"/>
+			<view style="display: flex; flex-direction: column; align-items: center;">
+				<input class="header-tit" type="text" placeholder="在此输入题目" v-model="data.name"/>
+				<input class="header-descr" type="text" placeholder="在此输入介绍" v-model="data.descr"/>
+			</view>
 			<view style="display: flex; flex-direction: row;">
 				<u-icon name="clock" style="padding: 7rpx 10rpx 0 0;" color="#98A1BB"></u-icon>
-				<!-- TODO时间选择器 -->
-				<input type="text" placeholder="截止时间" v-model="data.endTime"/>
+				<picker style="color: #98A1BB; font-size: 0.9rem; margin-top: 4rpx;" 
+					mode="date" :value="data.startTime" @change="setStartTime">{{data.startTime?data.startTime:"开始时间"}}</picker>
+				<view style="margin: 0 10rpx; color: #98A1BB;">-</view>
+				<picker style="color: #98A1BB; font-size: 0.9rem; margin-top: 4rpx;" 
+					mode="date" :value="data.endTime" @change="setEndTime">{{data.endTime?data.endTime:"开始时间"}}</picker>
 			</view>
 		</view>
 		<view v-for="(item, index) in data.questionList" :key="index">			
 			<editableQuestion :question="item">{{index + 1}}.</editableQuestion>
-			
 			<!-- 操作菜单 -->
 			<view class="item-bar">
 				<view style="display: flex; padding: 20rpx; border-top: 1px solid #22CDEE;">
@@ -27,7 +30,6 @@
 			</view>
 		</view>
 	</view>
-	<button class="btn" @click="save">保存</button>
 	<button class="btn" @click="deliver">发布</button>
 </template>
 
@@ -35,65 +37,48 @@
 import editableQuestion from '../../components/questionnaire/editableQuestion.vue'
 import {onBeforeMount, reactive} from 'vue'
 import {delLocalData, getLocalData, setLocalData} from '../../utils/cache.js'
+import { addQuestionnaire } from '../../api/questionnaire';
 
 const data = reactive({
-	id: 0,
-	name: '',
-	endTime: '',
+	id: "0",
+	type: 1,
+	descr: "",
+	name: "",
+	startTime: "",
+	endTime: "",
 	questionList: [
 		
 	]
 })
 
+// 时间选择器 时间设置
+const setStartTime = (n) =>{
+	data.startTime = n.detail.value
+}
+
+const setEndTime = (n) =>{
+	data.endTime = n.detail.value
+}
+
 // 返回 函数值 实现 深拷贝
-// 返回一个 默认问卷
+// 返回一个 默认问题
 const getDefaultItem = () =>{
 	let dm = {
-		flag: 0,
-		title: '',
-		descr: '',
-		data: [
-			{
-				name: '',
-				selected: false
-			},
-			{
-				name: '',
-				selected: false
-			},
-			{
-				name: '',
-				selected: false
-			}
-		]
+		id: "0",
+		type: 1,
+		name: "",
+		descr: "",
+		content: undefined,
+		questionnaire: ""
 	}
 	return dm
 }
 
 
 const load = () =>{
-	data.id = Date.now()
+	data.id = getLocalData("new-qeid") + ""
 	// 新建草稿
 	data.questionList.push(getDefaultItem())
-	// 读取草稿
-	if(getLocalData('questionnaire-draft')){
-		uni.showModal({
-			title: '提示',
-			content: '是否继续编辑上次内容?',
-			success: res =>{				
-				if(res.confirm){
-					let ld = getLocalData('questionnaire-draft')
-					data.questionList.pop()
-					data.questionList.push(...ld.questionList)
-					data.id = ld.id
-					data.name = ld.name
-					data.endTime = ld.endTime
-				} else{
-					delLocalData('questionnaire-draft')
-				}
-			}
-		})
-	}
 }
 
 onBeforeMount(() =>{
@@ -133,53 +118,42 @@ const downItem = (index) =>{
 	}
 }
 
-const save = () =>{
-	try {			
-		setLocalData('questionnaire-draft', data)
-		uni.showToast({
-			title: '保存成功!',
-			icon: 'success',
-			duration: 2000
-		})
-	} catch(e){
-		uni.showToast({
-			title: '请稍后重试',
-			icon: 'fail',
-			duration: 2000
-		})
-	}
-}
-
 const deliver = () =>{
 	uni.showModal({
 		title: '提示',
 		content: '是否确认发布?',
 		success: res =>{
 			if(res.confirm){
-				try{
-					let quesitonnaireList = getLocalData('questionnaire-list')?getLocalData('questionnaire-list'):[]
-					quesitonnaireList.push(data)
-					setLocalData('questionnaire-list', quesitonnaireList)
-					delLocalData('questionnaire-draft')
-					uni.showToast({
-						title: '发布成功!',
-						icon: 'success',
-						duration: 2000,
-						success: res =>{
-							setTimeout(() =>{								
-								uni.navigateBack({
-									delta: 1
-								})
-							}, 1000)
-						}
-					})
-				} catch(e){
+				addQuestionnaire(data).then(res =>{
+					if(res.code) {
+						uni.showToast({
+							title: '发布成功!',
+							icon: 'success',
+							duration: 2000,
+							success: res =>{
+								setTimeout(() =>{								
+									uni.navigateBack({
+										delta: 1
+									})
+								}, 1000)
+							}
+						})
+					} else {
+						uni.showToast({
+							title: '请稍后重试',
+							icon: 'loading',
+							duration: 2000
+						})
+					}
+				}).catch(err =>{
+					// 失败
 					uni.showToast({
 						title: '请稍后重试',
-						icon: 'fail',
+						icon: 'loading',
 						duration: 2000
 					})
-				}
+					return
+				})
 			} 
 		}
 	})
@@ -207,10 +181,21 @@ const deliver = () =>{
 		border-right: 2px solid #768BFF;
 		padding-left: 5rpx;
 		line-height: 40rpx;
-		margin-bottom: 50rpx;
+		margin-bottom: 30rpx;
 		font-size: 1.1rem;
 		color: #768BFF;
 		text-align: center;
+		width: 100%;
+	}
+	.header-descr{
+		border-left: 1px solid #333333;
+		border-right: 1px solid #333333;
+		margin-bottom: 50rpx;
+		font-size: 0.8rem;
+		color: #768BFF;
+		text-align: center;
+		word-wrap: break-word;
+		width: 500rpx;
 	}
 	.header{
 		width: 702rpx;
