7f00c263f4163d3a74ba7236aa3ad60b2741be14
远志 <93107257+yuanzhimengbian@users.noreply.github.com>
2024-02-26 20:45:23 +0800

    首页公告修改

--- pages/index/bulletin.vue
+++ pages/index/bulletin.vue
@@ -1,74 +1,106 @@
 <template>
-	<uni-section title="社区公告" sub-title="" type="line" style="width: 100%;">
-		<view class="notice-list">
-			<view class="notice-item" v-for="(item,index) in textArr" :key="index">
-				<text>{{item.title}}</text>
-				<text>发布时间：{{item.date}}</text>
+	<view>
+		<uni-section :title="tittle" sub-title="" type="line" style="width: 100%;margin: auto;">
+			<view class="notice-list">
+				<view class="notice-item" v-for="(item,index) in articles" :key="index" @click="todetail(item.articleId)">
+					<text style="text-align: center;">{{item.title}}</text>
+					<text>{{item.summary}}</text>
+					<text style="text-align: right;">发布人：{{item.author}}</text>
+					<text style="text-align: right;">发布时间：{{item.publishDate}}</text>
+				</view>
 			</view>
-		</view>
-	</uni-section>
+		</uni-section>
+	</view>
 </template>
 
 <script setup>
-	const textArr = [{
-			"title": "智慧社区：上线了！",
-			"url": "11",
-			"date": "2024-2-5 21:48"
-		},
-		{
-			"title": "祝愿社区各位居民欢度春节！",
-			"url": "22",
-			"date": "2024-1-6 13:50"
-		},
-		{
-			"title": "大雪预警",
-			"url": "333",
-			"date": "2023-11-11 11:11"
-		},
-	];
+	import { service } from "@/utils/request.js"
+	import { onBeforeMount, ref } from "vue";
+	import { onLoad, onShow } from "@dcloudio/uni-app";
 
-	function noticeBarClick(url) {
-		uni.switchTab({
-			url
+	const articles = ref([]);
+	const tittle = ref();
+	
+    async function fetchArticles(option) {
+      try {
+        const response = await service('/article/getList', 'GET' ,option);
+        if (Array.isArray(response.data)) {
+			articles.value = response.data;
+			if (response.data == null ) {
+				tittle = "页面不存在"
+			}
+        } else {
+          console.error('Invalid response format.');
+        }
+      } catch (error) {
+        console.error('Error fetching articles:', error);
+      }
+    }
+
+	onLoad(() => {
+		const option = {"categoryType" : "公告通知"};
+		fetchArticles(option);
+		tittle.value = option.categoryType;
+	})
+	
+	function todetail (articleId){
+		uni.navigateTo({
+			url:"/pages/notice/noticedetail?articleId="+articleId
 		})
-	};
+	}
 </script>
 
 <style>
 	.notice-list {
-		width: 100%
 	}
-
+	
 	.notice-item {
-		width: 93.5%;
-		height: calc(101rpx * 2);
+		width: 92.8%;
+		height: auto;
 		border: 1px solid #e2e2e2;
 		border-radius: calc(18rpx * 2);
 		box-sizing: border-box;
-		padding: 50rpx 30rpx;
+		padding: 10rpx 30rpx;
+		padding-top: 30rpx;
 		flex-shrink: 0;
-		margin: 24rpx;
+		margin-top: 24rpx!important;
+		margin-button: 24rpx;
 		font-family: 'Inter';
 		font-style: normal;
 		font-weight: 400;
 		font-size: calc(14rpx * 2);
 		line-height: calc(17rpx * 2);
 		color: #333333;
+		margin: auto;
+		
 	}
 
 	.notice-item>text {
 		display: block;
 	}
-
+	
 	.notice-item text:nth-child(2) {
+		text-indent: 2em;
 		display: flex;
-		justify-content: space-between;
+		padding-top: 10rpx;
+	}
+
+	.notice-item text:nth-child(3) {
+		padding-top: 0rpx;
+		font-size: 16rpx !important;
+	}
+	.notice-item text:nth-child(4) {
+		font-size: 16rpx !important;
+	}
+	
+	.notice-item text:nth-child(2),
+	.notice-item text:nth-child(3),
+	.notice-item text:nth-child(4) {
 		color: #999999;
 		font-family: 'Inter';
 		font-style: normal;
 		font-weight: 400;
 		font-size: calc(12rpx * 2);
 		line-height: calc(15rpx * 2);
-		padding-top: 38rpx;
 	}
 </style>
